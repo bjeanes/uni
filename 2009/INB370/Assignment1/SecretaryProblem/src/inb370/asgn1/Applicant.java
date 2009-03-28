4969bffe9ded4a2f7bb213412e7a92095e08e632
Bodaniel Jeanes <me@bjeanes.com>
2009-03-28 12:32:10 +1000

    Filled in stubs for all Applicant methods

--- 2009/INB370/Assignment1/SecretaryProblem/src/inb370/asgn1/Applicant.java
+++ 2009/INB370/Assignment1/SecretaryProblem/src/inb370/asgn1/Applicant.java
@@ -1,5 +1,25 @@
 package inb370.asgn1;
 
-public class Applicant {
+import java.lang.String;
 
+public class Applicant {
+	public Applicant(int id, double qualityScore) {
+		
+	}
+	
+	public int getId() {
+		return 0;
+	}
+	
+	public void setId(int id) {}
+	
+	public double getQualityScore() {
+		return 0.0;
+	}
+	
+	public void setQualityScore(double qualityScore) {}
+	
+	public String toString() {
+		return "";
+	}
 }
