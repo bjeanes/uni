e6950b1adc09e67744e4fd3e423ed33873449141
Bodaniel Jeanes <me@bjeanes.com>
2009-03-28 12:35:02 +1000

    Added HiringGame class that implements IHiringGame

---
+++ 2009/INB370/Assignment1/SecretaryProblem/src/inb370/asgn1/HiringGame.java
@@ -0,0 +1,44 @@
+package inb370.asgn1;
+
+import java.util.Random;
+
+public class HiringGame implements IHiringGame {
+
+	@Override
+	public void acceptApplicant() throws HiringException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Applicant getBestApplicant() throws HiringException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Applicant getNextApplicant() throws HiringException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAccepted() throws HiringException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isBestApplicant() throws HiringException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void newGame(int maxApplicants, Random random)
+			throws HiringException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
