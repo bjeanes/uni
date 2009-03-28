9a7daa4ec045154b5b3ab5e02c9395c5531747b8
Bodaniel Jeanes <me@bjeanes.com>
2009-03-28 12:51:41 +1000

    Made newGame method of HiringGame do something useful

--- 2009/INB370/Assignment1/SecretaryProblem/src/inb370/asgn1/HiringGame.java
+++ 2009/INB370/Assignment1/SecretaryProblem/src/inb370/asgn1/HiringGame.java
@@ -3,6 +3,9 @@ package inb370.asgn1;
 import java.util.Random;
 
 public class HiringGame implements IHiringGame {
+	private int maxApplicants;
+	private Random random;
+	private Applicant[] applicants;
 
 	@Override
 	public void acceptApplicant() throws HiringException {
@@ -37,7 +40,18 @@ public class HiringGame implements IHiringGame {
 	@Override
 	public void newGame(int maxApplicants, Random random)
 			throws HiringException {
-		// TODO Auto-generated method stub
+		if(maxApplicants < 1) 
+			throw new HiringException("There must be at least 1 applicant.");
+		if(random == null)
+			throw new HiringException("HiringGame requires a Random object.");
+		
+		this.maxApplicants = maxApplicants;
+		this.random = random;
+		this.applicants = new Applicant[maxApplicants];
+		
+		for(int i = 0; i < maxApplicants; i++) {
+			applicants[i] = new Applicant(i, random.nextDouble());
+		}
 
 	}
 
