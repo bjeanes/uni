84ea8cae56d49b86e97235d8a4c13cf3040d7dab
Bodaniel Jeanes <me@bjeanes.com>
2009-03-29 16:22:57 +1000

    Removed unused imports and applied code formatting

--- 2009/INB370/Assignment1/SecretaryProblem/src/inb370/asgn1/ApplicantUnitTest.java
+++ 2009/INB370/Assignment1/SecretaryProblem/src/inb370/asgn1/ApplicantUnitTest.java
@@ -2,7 +2,6 @@ package inb370.asgn1;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
--- 2009/INB370/Assignment1/SecretaryProblem/src/inb370/asgn1/HiringGameUnitTest.java
+++ 2009/INB370/Assignment1/SecretaryProblem/src/inb370/asgn1/HiringGameUnitTest.java
@@ -1,9 +1,7 @@
 package inb370.asgn1;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Random;
 
@@ -12,9 +10,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- *
+ * 
  * @author Bodaniel Jeanes
- *
+ * 
  */
 public class HiringGameUnitTest {
 	private HiringGame game;
@@ -31,26 +29,30 @@ public class HiringGameUnitTest {
 		game = new HiringGame();
 		random = new Random();
 	}
-	
-	@Test(expected=HiringException.class)
-	public void testNewGameThrowsAnExceptionWhenGivenNullRandomObject() throws HiringException {
+
+	@Test(expected = HiringException.class)
+	public void testNewGameThrowsAnExceptionWhenGivenNullRandomObject()
+			throws HiringException {
 		game.newGame(5, null);
 	}
-	
-	@Test(expected=HiringException.class)
-	public void testNewGameThrowsAnExceptionWhenGivenZeroMaxApplicant() throws HiringException {
+
+	@Test(expected = HiringException.class)
+	public void testNewGameThrowsAnExceptionWhenGivenZeroMaxApplicant()
+			throws HiringException {
 		game.newGame(0, random);
 	}
-	
-	@Test(expected=HiringException.class)
-	public void testNewGameThrowsAnExceptionWhenGivenNegativeMaxApplicant() throws HiringException {
+
+	@Test(expected = HiringException.class)
+	public void testNewGameThrowsAnExceptionWhenGivenNegativeMaxApplicant()
+			throws HiringException {
 		game.newGame(-1, random);
 	}
-	
+
 	@Test
 	public void testNewGameDoesNotThrowAnExceptionWhenGivenOneMaxApplicant() {
-		try { game.newGame(1, random); }
-		catch(HiringException e) {
+		try {
+			game.newGame(1, random);
+		} catch (HiringException e) {
 			AssertionError ae = new AssertionError();
 			ae.initCause(e);
 			throw ae;
@@ -59,38 +61,41 @@ public class HiringGameUnitTest {
 
 	@Test
 	public void testNewGameDoesNotThrowAnExceptionWhenGivenNormalAmountOfMaxApplicant() {
-		try { game.newGame(100, random); }
-		catch(HiringException e) {
+		try {
+			game.newGame(100, random);
+		} catch (HiringException e) {
 			AssertionError ae = new AssertionError();
 			ae.initCause(e);
 			throw ae;
 		}
 	}
-	
-	@Test(expected=HiringException.class)
-	public void testGetNextApplicantThrowsExceptionWhenGameHasNotStarted() throws HiringException {
+
+	@Test(expected = HiringException.class)
+	public void testGetNextApplicantThrowsExceptionWhenGameHasNotStarted()
+			throws HiringException {
 		game.getNextApplicant();
 	}
-	
-	@Test(expected=HiringException.class)
-	public void testGetNextApplicantThrowsExceptionWhenApplicantHasBeenAccepted() throws HiringException {
+
+	@Test(expected = HiringException.class)
+	public void testGetNextApplicantThrowsExceptionWhenApplicantHasBeenAccepted()
+			throws HiringException {
 		try {
 			game.newGame(10, random);
 			game.getNextApplicant();
 			game.acceptApplicant();
-		} catch(HiringException e) {
+		} catch (HiringException e) {
 			AssertionError ae = new AssertionError();
 			ae.initCause(e);
 			throw ae;
 		}
-		
-		// Only is a success if this call raises a HiringException		
+
+		// Only is a success if this call raises a HiringException
 		game.getNextApplicant();
 	}
-	
+
 	/**
-	 * This tests that if an applicant has been accepted using {@code acceptApplicant()},
-	 * isAccepted will return {@code true}.
+	 * This tests that if an applicant has been accepted using
+	 * {@code acceptApplicant()}, isAccepted will return {@code true}.
 	 */
 	@Test
 	public void testIsAcceptedReturnsTrueWhenAnApplicantHasBeenAccepted() {
@@ -99,15 +104,32 @@ public class HiringGameUnitTest {
 			game.getNextApplicant();
 			game.acceptApplicant();
 			assertTrue(game.isAccepted());
-		} catch(HiringException e) {
+		} catch (HiringException e) {
 			AssertionError ae = new AssertionError();
 			ae.initCause(e);
 			throw ae;
 		}
 	}
-	
+
 	/**
-	 * This tests that the game will immediately accept a single applicant. 
+	 * This tests that if an applicant has not been accepted using
+	 * {@code acceptApplicant()}, isAccepted will return {@code false}.
+	 */
+	@Test
+	public void testIsAcceptedReturnsFalseWhenAnApplicantHasNotBeenAccepted() {
+		try {
+			game.newGame(5, random);
+			game.getNextApplicant();
+			assertFalse(game.isAccepted());
+		} catch (HiringException e) {
+			AssertionError ae = new AssertionError();
+			ae.initCause(e);
+			throw ae;
+		}
+	}
+
+	/**
+	 * This tests that the game will immediately accept a single applicant.
 	 */
 	@Test
 	public void testIsAcceptedImmediatelyIfOnlyOneApplicant() {
