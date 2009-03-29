d9bd1bcdf916c84471865a502863615f62e3f657
Bodaniel Jeanes <me@bjeanes.com>
2009-03-29 22:32:41 +1000

    Formatted and commented the Applicant class

--- 2009/INB370/Assignment1/SecretaryProblem/src/inb370/asgn1/Applicant.java
+++ 2009/INB370/Assignment1/SecretaryProblem/src/inb370/asgn1/Applicant.java
@@ -3,32 +3,46 @@ package inb370.asgn1;
 public class Applicant {
 	private int id;
 	private double qualityScore;
-	
+
 	public Applicant(int id, double qualityScore) {
 		setId(id);
 		setQualityScore(qualityScore);
 	}
-	
+
 	public int getId() {
 		return id;
 	}
-	
+
 	public double getQualityScore() {
 		return qualityScore;
 	}
-	
+
 	public void setId(int id) {
 		this.id = id;
 	}
-	
-	public void setQualityScore(double qualityScore) 
+
+	/**
+	 * This sets the quality score if it is within an acceptable range (0.0 <=
+	 * qualityScore <= 1.0), else raises an exception
+	 * 
+	 * @param qualityScore
+	 * @throws IllegalArgumentException
+	 */
+	public void setQualityScore(double qualityScore)
 			throws IllegalArgumentException {
-		if(qualityScore > 1.0 || qualityScore < 0.0) throw new IllegalArgumentException();
+		if (qualityScore > 1.0 || qualityScore < 0.0)
+			throw new IllegalArgumentException();
 		this.qualityScore = qualityScore;
 	}
+
 	
+	/**
+	 * Shows the applicant ID and the quality score as a percentage with an accuracy of 2
+	 * 
+	 * @return String - e.g. "Applicant #123 (12.34%)"
+	 */
 	public String toString() {
-		return "Applicant #" + Integer.toString(getId()) + 
-			" (" + String.format("%1$.2f%%", getQualityScore() * 100.0) + ")";
+		return "Applicant #" + Integer.toString(getId()) + " ("
+				+ String.format("%1$.2f%%", getQualityScore() * 100.0) + ")";
 	}
 }
